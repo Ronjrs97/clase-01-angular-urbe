c0363a6b97b97cc1c15e65714d352bf81f7eca21
ronaldo <ronjrs97@gmail.com>
2025-07-19 10:07:26 -0400

    Signals

--- src/app/tasks-manager/pages/task-manager-page/task-manager-page.component.ts
+++ src/app/tasks-manager/pages/task-manager-page/task-manager-page.component.ts
@@ -1,8 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   imports: [],
   templateUrl: './task-manager-page.component.html',
   styleUrl: './task-manager-page.component.css',
 })
-export default class TaskManagerPageComponent { }
+export default class TaskManagerPageComponent {
+
+  tasks = signal(0);
+  // task: number = 0;
+  seconds = signal(0);
+
+  constructor(){
+     setInterval(() => {
+      // this.counter += 1;
+      this.seconds.update( (current) => current + 1);
+    }, 1000)
+  }
+
+  completeTask(){
+    this.tasks.update((currentValue) => currentValue + 1)
+    // this.task =+ 1;
+  }
+
+  //* Deshacer la tarea
+  undoTask(value: number = 1){
+    if (this.tasks() == 0) return;
+    // if (this.tasks() == 0) {
+    //   return;
+    // }
+    // if (this.tasks() > 0) {
+      this.tasks.update( (current) => current - value);
+    // }
+  }
+
+  resetTask(){
+    this.tasks.set(0);
+  }
+
+ }
